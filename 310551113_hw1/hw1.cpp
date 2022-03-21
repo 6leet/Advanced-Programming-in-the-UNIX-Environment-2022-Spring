02a1655406fd50a89977c4c61764817bf78cc54f
leolee0118 <leo0118hsnu@gmail.com>
2022-03-21 23:14:25 +0800

    iterate /proc

--- 310551113_hw1/hw1.cpp
+++ 310551113_hw1/hw1.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <filesystem>
 
 using namespace std;
 
 struct filter {
     regex command;
     regex filename;
-    string type;
+    regex type;
     filter() {
         command = regex(".*");
         filename = regex(".*");
-        type = "";
+        type = regex(".*");
+    }
+    bool process(string _command, string _filename, string _type) {
+        return regex_search(_command, command) && regex_search(_filename, filename) && regex_match(_type, type);
     }
 };
 
-int main(int argc, char *argv[]) {
+filter setFilter(int argc, char *argv[]) {
     filter f;
     for (int i = 0; i < argc; i++) {
         if (string(argv[i]) == "-c") {
-            
+            f.command = regex(argv[i++]);
         } else if (string(argv[i]) == "-t") {
-            // f.filenameRegex = argv[i++];
+            f.filename = regex(argv[i++]);
         } else if (string(argv[i]) == "-f") {
-            // f.typeFilter = argv[i++];
+            f.type = regex(argv[i++]);
         }
     }
-    while (true) {
-        string s;
-        smatch m;
-        cin >> s;
-        cout << regex_search(s, m, f.command) << '\n';
+    return f;
+}
+
+void iterate() {
+    filesystem::path path{"/proc"};
+    for (auto const& entry : filesystem::directory_iterator{path}) {
+        if (entry.is_directory()) {
+            cout << filesystem::absolute(entry.path()).string() << '\n';
+        }
     }
+}
+
+int main(int argc, char *argv[]) {
+    setFilter(argc, argv);
+    iterate();
 }
