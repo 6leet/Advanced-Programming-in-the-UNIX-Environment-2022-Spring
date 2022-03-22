cc45d10b3934ea83639fe3a00f3b2d61905688c6
Leo Lee <leo0118hsnu@gmail.com>
2022-03-22 13:40:15 +0800

    start filling file & about to test permission issue

--- 310551113_hw1/hw1.cpp
+++ 310551113_hw1/hw1.cpp
@@ -63,10 +63,16 @@ struct Process {
         files.clear();
     }
     void show() {
-        cout << setw(maxlen.command + 4) << left << command 
-            << setw(maxlen.pid + 4) << left << pid 
-            << setw(maxlen.user + 4) << left << user 
-            << '\n';
+        for (int i = 0; i < files.size(); i++) {
+            cout << setw(maxlen.command + 4) << left << command 
+                << setw(maxlen.pid + 4) << left << pid 
+                << setw(maxlen.user + 4) << left << user 
+                << setw(maxlen.fd + 4) << left << files[i].fd
+                << setw(maxlen.type) << left << files[i].type
+                << setw(maxlen.node) << left << files[i].node
+                << setw(maxlen.name) << left << files[i].name
+                << '\n';
+        }
     }
 };
 
@@ -83,6 +89,19 @@ struct Filter {
         return regex_search(_command, command) && regex_search(_filename, filename) && regex_match(_type, type);
     }
 };
+
+void updateMax(Process &proc) {
+    maxlen.command = max(maxlen.command, int(proc.command.size()));
+    maxlen.pid = max(maxlen.pid, int(to_string(proc.pid).size()));
+    maxlen.user = max(maxlen.user, int(proc.user.size()));
+
+    for (int i = 0; i < proc.files.size(); i++) {
+        maxlen.fd = max(maxlen.fd, int(proc.files[i].fd.size()));
+        maxlen.type = max(maxlen.type, int(proc.files[i].type.size()));
+        maxlen.node = max(maxlen.node, int(proc.files[i].node.size()));
+        maxlen.name = max(maxlen.name, int(proc.files[i].name.size()));
+    }
+}
 
 Filter setFilter(int argc, char *argv[]) {
     Filter f;
@@ -137,12 +156,26 @@ void iterateProcess(string procPath, Process &proc) {
     for (auto const& entry : filesystem::directory_iterator{path}) {
         string procEntryA = filesystem::absolute(entry.path()).string();
         string procEntryF = filesystem::absolute(entry.path()).filename();
-        if (procEntryF == "exe") { // COMMAND
+        if (procEntryF == "exe") { 
+            // COMMAND
             proc.command = filesystem::read_symlink(entry.path()).filename();
-            maxlen.command = max(maxlen.command, int(proc.command.size()));
-        } else if (procEntryF == "status") {
+            
+
+            // FD: txt
+            File file;
+            file.fd = "txt";
+            file.type = "REG";
+            file.name = filesystem::read_symlink(entry.path()).string();
+            proc.files.push_back(file);
+        } else if (procEntryF == "status") { // USER
             proc.user = getProcUser(procEntryA);
-            maxlen.user = max(maxlen.user, int(proc.user.size()));
+            
+        } else if (procEntryF == "cwd") { // FD: current working directory
+            File file;
+            file.fd = "cwd";
+            file.type = "DIR";
+            file.name = filesystem::read_symlink(entry.path()).string();
+            proc.files.push_back(file);
         }
     }
 }
@@ -155,8 +188,8 @@ void iterateBase(string basePath, vector<Process> &processes) {
             string procPathA = filesystem::absolute(entry.path()).string();
             if (isNumber(procPathR)) {
                 Process proc(stoi(procPathR));
-                maxlen.pid = max(maxlen.pid, int(to_string(proc.pid).size()));
                 iterateProcess(procPathA, proc);
+                updateMax(proc);
                 processes.push_back(proc);
             }
         }
@@ -167,6 +200,10 @@ void output(vector<Process> processes) {
     cout << setw(maxlen.command + 4) << left << "COMMAND" 
         << setw(maxlen.pid + 4) << left << "PID" 
         << setw(maxlen.user + 4) << left << "USER" 
+        << setw(maxlen.fd + 4) << left << "FD"
+        << setw(maxlen.type) << left << "TYPE"
+        << setw(maxlen.node) << left << "NODE"
+        << setw(maxlen.name) << left << "NAME"
         << '\n';
     for (int i = 0; i < processes.size(); i++) {
         processes[i].show();
