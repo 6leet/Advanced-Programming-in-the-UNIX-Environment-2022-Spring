4f36328a095f269640c1a37ec0ce0e0e0b54188d
leolee0118 <leo0118hsnu@gmail.com>
2022-03-23 20:50:38 +0800

    handle stopped process

--- 310551113_hw1/hw1.cpp
+++ 310551113_hw1/hw1.cpp
@@ -210,7 +210,7 @@ string getMode(string filename) {
     } else if (_mode >= 2) {
         mode = "w";
     }
-    return " " + to_string(buf.st_mode);
+    return filename + " " + to_string(buf.st_mode);
 }
 
 int getInode(string filename) {
